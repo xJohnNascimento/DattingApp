edc0e52b1353e79f9be3b8e7f6a0528dcbf73a38
JohnNascimento <xjohn.nascimento@gmail.com>
2021-02-02 09:42:16 -0300

    End of section 4

---
+++ API/Data/Migrations/20210131232159_UserPasswordAdded.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace API.Data.Migrations
+{
+    public partial class UserPasswordAdded : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<byte>(
+                name: "UserName",
+                table: "User",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: (byte)0,
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<byte[]>(
+                name: "PasswordHash",
+                table: "User",
+                type: "BLOB",
+                nullable: true);
+
+            migrationBuilder.AddColumn<byte[]>(
+                name: "PasswordSalt",
+                table: "User",
+                type: "BLOB",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PasswordHash",
+                table: "User");
+
+            migrationBuilder.DropColumn(
+                name: "PasswordSalt",
+                table: "User");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "UserName",
+                table: "User",
+                type: "TEXT",
+                nullable: true,
+                oldClrType: typeof(byte),
+                oldType: "INTEGER");
+        }
+    }
+}
